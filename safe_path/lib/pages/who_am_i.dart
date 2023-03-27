03a5346a27b71661d8329e874c7b17ed769d8e05
Eyad Hussein <eyadhussein952018@gmail.com>
2023-03-27 07:11:04 +0200

    imported and rendered svg

--- safe_path/lib/pages/who_am_i.dart
+++ safe_path/lib/pages/who_am_i.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import '../widgets/avatars.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class WhoAmI extends StatelessWidget {
   const WhoAmI({super.key});
@@ -26,21 +25,20 @@ class WhoAmI extends StatelessWidget {
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      svgg()
-                      // Container(
-                      //   decoration: BoxDecoration(
-                      //       color: Color.fromRGBO(32, 147, 238, 1),
-                      //       borderRadius: BorderRadius.all(Radius.circular(7))),
-                      //   height: 63,
-                      //   width: 190,
-                      //   child: Center(
-                      //     child: Text("Sign As",
-                      //         style: TextStyle(
-                      //             fontSize: 42,
-                      //             fontWeight: FontWeight.bold,
-                      //             color: Colors.white)),
-                      //   ),
-                      // ),
+                      Container(
+                        decoration: BoxDecoration(
+                            color: Color.fromRGBO(32, 147, 238, 1),
+                            borderRadius: BorderRadius.all(Radius.circular(7))),
+                        height: 63,
+                        width: 190,
+                        child: Center(
+                          child: Text("Sign As",
+                              style: TextStyle(
+                                  fontSize: 42,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white)),
+                        ),
+                      ),
                     ],
                   ),
                   Spacer(),
@@ -57,146 +55,5 @@ class WhoAmI extends StatelessWidget {
             ],
           ),
         ));
-  }
-
-  Container svgg() {
-    return Container(
-      width: 123.800537109375,
-      height: 344.8099365234375,
-      child: Stack(
-        children: <Widget>[
-          Positioned(
-              top: 333.6668701171875,
-              left: 15.18306827545166,
-              child: Container(
-                  width: 108.61746978759766,
-                  height: 11.143054962158203,
-                  child: Stack(children: <Widget>[
-                    Positioned(
-                        top: 0,
-                        left: 0,
-                        child: Container(
-                            width: 108.61746978759766,
-                            height: 11.143054962158203,
-                            child: Stack(children: <Widget>[
-                              Positioned(
-                                top: 0,
-                                left: 0,
-                                child: SvgPicture.asset(
-                                    'assets/images/vector.svg',
-                                    semanticsLabel: 'vector'),
-                              ),
-                            ]))),
-                  ]))),
-          Positioned(
-            top: 333.6668701171875,
-            left: 15.18306827545166,
-            child: SvgPicture.asset('assets/images/vector_2.svg',
-                semanticsLabel: 'vector_2'),
-          ),
-          Positioned(
-            top: 0,
-            left: 0,
-            child: Container(
-              width: 97.79618072509766,
-              height: 342.5264587402344,
-              child: Stack(
-                children: <Widget>[
-                  Positioned(
-                    top: 49.67081832885742,
-                    left: -9.278993502448429e-7,
-                    child: SvgPicture.asset('assets/images/vector_3.svg',
-                        semanticsLabel: 'vector_3'),
-                  ),
-                  Positioned(
-                      top: -0.000004536396772891749,
-                      left: 7.54596471786499,
-                      child: Container(
-                          width: 90.25021362304688,
-                          height: 342.5264587402344,
-                          child: Stack(children: <Widget>[
-                            Positioned(
-                              top: 147.1020965576172,
-                              left: 12.376644134521484,
-                              child: SvgPicture.asset(
-                                  'assets/images/vector_4.svg',
-                                  semanticsLabel: 'vector_4'),
-                            ),
-                            Positioned(
-                              top: 48.358299255371094,
-                              left: 6.789455890655518,
-                              child: SvgPicture.asset(
-                                  'assets/images/vector_5.svg',
-                                  semanticsLabel: 'vector_5'),
-                            ),
-                            Positioned(
-                              top: 1.793736219406128,
-                              left: 26.765796661376953,
-                              child: SvgPicture.asset(
-                                  'assets/images/xmlid107.svg',
-                                  semanticsLabel: 'xmlid107'),
-                            ),
-                            Positioned(
-                              top: -0.000004536396772891749,
-                              left: 19.51348114013672,
-                              child: SvgPicture.asset(
-                                  'assets/images/xmlid103.svg',
-                                  semanticsLabel: 'xmlid103'),
-                            ),
-                            Positioned(
-                              top: 61.731266021728516,
-                              left: 0.0000014433990145334974,
-                              child: SvgPicture.asset(
-                                  'assets/images/xmlid102.svg',
-                                  semanticsLabel: 'xmlid102'),
-                            ),
-                            Positioned(
-                              top: 61.775001525878906,
-                              left: 0.811198890209198,
-                              child: SvgPicture.asset(
-                                  'assets/images/xmlid101.svg',
-                                  semanticsLabel: 'xmlid101'),
-                            ),
-                            Positioned(
-                              top: 94.5436782836914,
-                              left: 27.033782958984375,
-                              child: SvgPicture.asset(
-                                  'assets/images/xmlid100.svg',
-                                  semanticsLabel: 'xmlid100'),
-                            ),
-                            Positioned(
-                              top: 65.59587860107422,
-                              left: 53.3011589050293,
-                              child: SvgPicture.asset(
-                                  'assets/images/xmlid99.svg',
-                                  semanticsLabel: 'xmlid99'),
-                            ),
-                            Positioned(
-                              top: 82.39581298828125,
-                              left: 18.32854461669922,
-                              child: SvgPicture.asset(
-                                  'assets/images/xmlid98.svg',
-                                  semanticsLabel: 'xmlid98'),
-                            ),
-                          ]))),
-                  Positioned(
-                    top: 75.0020751953125,
-                    left: 8.037242889404297,
-                    child: SvgPicture.asset('assets/images/vector_6.svg',
-                        semanticsLabel: 'vector_6'),
-                  ),
-                  Positioned(
-                    top: 49.64165115356445,
-                    left: 39.73044204711914,
-                    child: SvgPicture.asset('assets/images/vector_7.svg',
-                        semanticsLabel: 'vector_7'),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
   }
 }
--- safe_path/lib/widgets/avatars.dart
+++ safe_path/lib/widgets/avatars.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class MainAvatar extends StatelessWidget {
   final String content;
@@ -20,18 +21,52 @@ class MainAvatar extends StatelessWidget {
                   fontSize: 36,
                   fontWeight: FontWeight.bold),
             ),
+            SizedBox(
+              height: 168,
+              width: 168,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  alignment: Alignment.bottomCenter,
+                  padding: EdgeInsets.only(left: 15),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(100),
+                  ),
+                  backgroundColor: Color.fromRGBO(32, 147, 238, 0.7),
+                ),
+                onPressed: () {},
+                child: SvgPicture.asset(
+                  "images/pavan3841.svg",
+                ),
+              ),
+            )
+
             // Stack(
             // children: [
-            Stack(children: [
-              Container(
-                margin: EdgeInsets.only(top: 10),
-                height: 168,
-                width: 168,
-                decoration: BoxDecoration(
-                    color: Color.fromRGBO(32, 147, 238, 0.7),
-                    borderRadius: BorderRadius.all(Radius.circular(100))),
-              ),
-            ]),
+            // Stack(
+            //   alignment: Alignment.bottomCenter,
+            //   children: [
+            //     Container(
+            //       margin: EdgeInsets.only(top: 10),
+            //       height: 168,
+            //       width: 168,
+            //       decoration: BoxDecoration(
+            //         color: Color.fromRGBO(32, 147, 238, 0.7),
+            //         borderRadius: BorderRadius.all(
+            //           Radius.circular(100),
+            //         ),
+            //       ),
+            //     ),
+            //     ElevatedButton(
+            //       style: ElevatedButton.styleFrom(
+            //           backgroundColor: Colors.transparent),
+            //       onPressed: () {},
+            //       child: Image.asset(
+            //         "images/logo.png",
+            //         height: 60,
+            //       ),
+            //     ),
+            //   ],
+            // ),
             //   ],
             // )
           ],
